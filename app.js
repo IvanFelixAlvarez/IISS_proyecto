42a6d912be977e6221bcb1e0aea33e2747fe8a3c
IvanFelixAlvarez <ivanfelixalvarez@hotmail.com>
2014-02-05 20:30:17 +0100

    Añadida la conexión a la base de datos y añadido el footer

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var db = []; //Para tener las notas.
+
+var util = require('util');
+var mysql = require('mysql');
+connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : ''
+});
 
 var app = express();
 
@@ -33,7 +42,17 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+
+app.post('/procesaformulario', function(req, res) {
+
+	/*Escribe en la base de datos */
+	var post  = {asunto: req.body.asunto, autor: req.body.autor, contenido: req.body.contenido};
+	var query = connection.query('INSERT INTO Proyecto.notas SET ?', post, function(err, result) {
+  		if(err) console.log(err);
+	});
+    res.redirect('/');
+
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
