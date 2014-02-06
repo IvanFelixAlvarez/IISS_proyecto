6813c09adac4d45329825c6c23ca31ecbed91af4
IvanFelixAlvarez <ivanfelixalvarez@hotmail.com>
2014-02-06 12:15:56 +0100

    Añadido socket de prueba. No funciona.

--- app.js
+++ app.js
@@ -4,10 +4,13 @@
  */
 
 var express = require('express');
+var app = express();
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
 var db = []; //Para tener las notas.
 
 var util = require('util');
@@ -17,9 +20,6 @@ connection = mysql.createConnection({
 	user     : 'root',
 	password : ''
 });
-
-var app = express();
-
 /* nombre del base de datos Proyecto
  * nombre del tabla: notas
  * attributos: id, asunto, autor, contenido, time */
@@ -43,6 +43,8 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 
+//Peticiones a la base de datos.
+
 app.post('/procesaformulario', function(req, res) {
 
 	/*Escribe en la base de datos */
@@ -54,6 +56,14 @@ app.post('/procesaformulario', function(req, res) {
 
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+io.sockets.on('connection', function (socket) {
+	"use strict";
+
+	//Solicitud de las notas a la base de datos.
+	socket.on('prueba', function (data) {
+
+		console.log("Socket de prueba");
+
+	});
+
 });
