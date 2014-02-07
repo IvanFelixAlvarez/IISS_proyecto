f96e1488613f29deb372fb21f7aff37a0ca558b7
thomasZen <thomas.zenkel(at)googlemail.com>
2014-02-07 19:59:49 +0100

    Estilo añadido

--- public/javascripts/functionality.js
+++ public/javascripts/functionality.js
@@ -4,10 +4,20 @@ $(document).ready(function() {
 var socket = io.connect();
 
 $('#contenedornotas').hide();	
+$('#modificadornotas').hide();
+
+$('#formularionotas').submit(function(){
+	if($('#asunto').val() == '' || $('#autor').val() == ''){
+		alert('Rellene los campos obligatorios: Autor y Asunto.');
+		return false;
+	}
+	return true;
+});
 
 $('#crearnota').click(function() {
 	$('#formularionotas').show();
 	$('#contenedornotas').hide();
+	$('#modificadornotas').hide();
 });
 
 $("#vernotas").bind("click", function(){
@@ -18,8 +28,8 @@ $('#buscarnotas').bind('click', function(){
 });
 
 socket.on('listadoNotas', function (db) {
-		var cadena="";
-		for(var i=0; i<db.length; ++i){
+		var cadena = "";
+		for(var i = 0; i < db.length; ++i){
 			cadena += "<div class=\"decoradornota\"><a class=\"vercontenido\" id=\"" + 
 				i + "\" href=\"javascript:void(0)\">VER</a><a class=\"modificarcontenido\" id=\"" + 
 				i + "\" href=\"javascript:void(0)\">MODIFICAR</a><strong>" + db[i].time.substring(0,10) + 
@@ -31,17 +41,49 @@ socket.on('listadoNotas', function (db) {
 		socket.emit('notasmostradas',{});
 
 		$('#formularionotas').hide();
+	    $('#modificadornotas').hide();
 		$('#contenedornotas').show();
 
 		$('.vercontenido').click(function(){
-			alert('Se ha pulsado '+$(this).attr('id'));
-			socket.emit('obtenerNota', {id : $(this).attr('id')});
-		});		
+			alert('Se ha pulsado '+ $(this).attr('id'));
+			socket.emit('obtenerNota', {id: $(this).attr('id')});
+		});	
+
+        $('.modificarcontenido').click(function(){
+            socket.emit('obtenerNotaModificar', {id: $(this).attr('id')});
+		});	
 });
 
 socket.on('darnota', function (db) {
-			alert(db.nota.autor);
+			$('#popup').fadeIn('slow');
+			$(window).fadeTo("slow",0.8);
+			$('#close').click(function(){
+		    $('#popup').fadeOut('slow');
+				return false;
+			});
 });
 
+
+socket.on('modificarNota', function (db) {
+    var cadena = "";
+    cadena = "<form id=\"modificadornota\" action=\"/procesamodificador\" method=\"post\" name=\"modificador\">" +
+             "<span>Asunto: </span><input type=\"text\" name=\"asunto\" data-tipo=\"asunto\" value=\"" + db.nota.asunto + "\">" +
+             "<span>Autor: </span><input type=\"text\" name=\"autor\" data-tipo=\"autor\" value=\"" + db.nota.autor + "\">" +
+             "<input type=\"text\" name=\"id\" data-tipo=\"id\" value=\"" + db.nota.id + "\" readonly>" + 
+             "<textarea name=\"contenido\">" + db.nota.contenido + "</textarea>" + 
+             "<input type=\"submit\" value=\"Modificar\" id=\"modificado\" data-tipo=\"enviar\">" +
+             "</form>";
+    $('#modificadornotas').html(cadena);
+    $('#modificado').click(function () {
+    	alert('Nota modificada con éxito.');
+    });
+    $('#formularionotas').hide();
+    $('#contenedornotas').hide();
+    $('#modificadornotas').show();
+});
+
+socket.on('notaModificada', function (data) {
+	alert('Nota modificada con éxito.');
+});
 
 });
